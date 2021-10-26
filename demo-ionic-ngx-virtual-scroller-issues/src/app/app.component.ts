30016ebe98b78eb892ebf2274ebdf0d69064c06a
Julian Poyourow <julian@poyourow.com>
2021-10-26 23:30:44 +0000

    Re-create problem

--- demo-ionic-ngx-virtual-scroller-issues/src/app/app.component.ts
+++ demo-ionic-ngx-virtual-scroller-issues/src/app/app.component.ts
@@ -1,18 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+export class AppComponent implements AfterViewInit {
+  labels = [];
+  selectedLabels = [];
+
+  recipes = [];
+  recipeFetchBuffer = 25;
+  fetchPerPage = 50;
+  lastRecipeCount = 0;
+  totalRecipeCount: number;
+
+  @ViewChild('contentContainer', { static: true }) contentContainer;
+  scrollElement;
+
+  constructor() {
+    this.loadRecipes(0, this.fetchPerPage);
+  }
+
+  ngAfterViewInit() {
+    this.getScrollElement();
+  }
+
+  async getScrollElement() {
+    this.scrollElement = await this.contentContainer.getScrollElement();
+  }
+
+  fetchMoreRecipes(event) {
+    const shouldFetchMore = this.lastRecipeCount < event.endIndex + this.recipeFetchBuffer;
+
+    const moreToScroll = this.lastRecipeCount <= this.totalRecipeCount;
+    if (shouldFetchMore && moreToScroll) {
+      this.loadRecipes(this.lastRecipeCount, this.fetchPerPage);
+    }
+  }
+
+  async loadRecipes(offset, numToFetch) {
+    this.lastRecipeCount += numToFetch;
+
+    const response = await fetch(
+      `https://api.recipesage.com/recipes/by-page?folder=main&userId=2e4ed3ea-019f-45f2-a489-7c23554dcf06&sort=-title&count=${numToFetch}&offset=${offset}`
+    ).then(response => response.json());
+
+    this.totalRecipeCount = response.totalCount;
+
+    this.recipes = this.recipes.concat(response.data);
+  }
+
 }
--- demo-ionic-ngx-virtual-scroller-issues/src/app/app.module.ts
+++ demo-ionic-ngx-virtual-scroller-issues/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, VirtualScrollerModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
